80af2b46513d957848168c3ebb8ffd19d05f3e7f
bayat <sara.bayat1916@gmail.com>
2025-10-25 00:01:29 +0330

    my second commit.

--- helloWorld.java
+++ helloWorld.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
+
 public class helloWorld{
 
     public static void main(String[] args) {
         System.out.println("Hello World");
 
+
+        Scanner sc = new Scanner(System.in, "UTF-8");
+
+        System.out.print("اسم را وارد کنید: ");
+        String firstName = sc.nextLine().trim();
+
+        System.out.print("فامیلی را وارد کنید: ");
+        String lastName = sc.nextLine().trim();
+
+        System.out.println("نام کامل: " + firstName + " " + lastName);
+
+        sc.close();
     }
 }
