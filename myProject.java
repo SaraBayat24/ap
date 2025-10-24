799ff08c4934ff8604e2a31958a25ae9939c93ae
bayat <sara.bayat1916@gmail.com>
2025-10-25 00:09:58 +0330

    sudoku.

---
+++ myProject.java
@@ -0,0 +1,14 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class myProject {
+
+    static int[][] board = new int[9][9];
+
+    public static void main(String[] args) throws Exception {
+
+
+
+
+
+    }}
