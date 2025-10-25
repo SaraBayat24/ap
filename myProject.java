31c8c79deaea4342d4c682b2c833c25650404e45
bayat <sara.bayat1916@gmail.com>
2025-10-26 00:24:40 +0330

    E_5_18

--- myProject.java
+++ myProject.java
@@ -1,48 +1,46 @@
 import java.util.Scanner;
 
 public class myProject {
-
     public static void main(String[] args) {
 
 
-            Scanner input = new Scanner(System.in);
 
-            System.out.print("Enter your income : ");
-            double income = input.nextDouble();
-            double tax = 0;
+        Scanner input = new Scanner(System.in);
 
 
-            if (income <= 50000) {
-                tax = income * 0.01;
-            }
+        System.out.println("Enter three strings: ");
+        String s1 = input.next();
+        String s2 = input.next();
+        String s3 = input.next();
 
-            else if (income <= 75000) {
-                tax = 50000 * 0.01 + (income - 50000) * 0.02;
-            }
+        String temp;
+
+        if (s1.compareTo(s2) > 0) {
+            temp = s1;
+            s1 = s2;
+            s2 = temp;
+        }
+
+        if (s2.compareTo(s3) > 0) {
+            temp = s2;
+            s2 = s3;
+            s3 = temp;
+        }
+
+        if (s1.compareTo(s2) > 0) {
+            temp = s1;
+            s1 = s2;
+            s2 = temp;
+        }
+
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+
+        input.close();
 
 
-            else if (income <= 100000) {
-                tax = 50000 * 0.01 + 25000 * 0.02 + (income - 75000) * 0.03;
-            }
 
-            else if (income <= 250000) {
-                tax = 50000 * 0.01 + 25000 * 0.02 + 25000 * 0.03 + (income - 100000) * 0.04;
-            }
-
-            else if (income <= 500000) {
-                tax = 50000 * 0.01 + 25000 * 0.02 + 25000 * 0.03 + 150000 * 0.04 + (income - 250000) * 0.05;
-            }
-
-            else if (income > 500000) {
-                tax = 50000 * 0.01 + 25000 * 0.02 + 25000 * 0.03 + 150000 * 0.04 + 250000 * 0.05 + (income - 500000) * 0.06;
-
-
-
-            }
-
-            System.out.println("Your tax is : " + tax);
-
-            input.close();
 
 
         }}
