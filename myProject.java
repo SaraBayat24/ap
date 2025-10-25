8322384d4d47fd22c490f21fa711556245bbafc6
bayat <sara.bayat1916@gmail.com>
2025-10-26 00:12:23 +0330

    E_5_15

--- myProject.java
+++ myProject.java
@@ -1,43 +1,48 @@
-// Write a program that reads a floating-point number and prints “zero” if the number
-//is zero. Otherwise, print “positive” or “negative”. Add “small” if the absolute value
-//of the number is less than 1, or “large” if it exceeds 1,000,000.
-
-
 import java.util.Scanner;
 
 public class myProject {
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
-        System.out.print("Enter floating-point number : ");
-        double x = input.nextDouble();
-
-        if (x == 0){
-            System.out.print("the number you entered is zero ");
-        }
-          else if (x > 0){
-            System.out.print("the number you entered is positive ");}
-
-            else {
-                System.out.print("the number you entered is negative ");
-              }
-
-        if ( x < 1){
-            System.out.print("  small ");
-        }
 
 
-        if(x < 0){ x = x * -1;}
+            Scanner input = new Scanner(System.in);
+
+            System.out.print("Enter your income : ");
+            double income = input.nextDouble();
+            double tax = 0;
 
 
-        if (x > 1000000){
-            System.out.print("larg ");
-        }
+            if (income <= 50000) {
+                tax = income * 0.01;
+            }
 
-input.close();
+            else if (income <= 75000) {
+                tax = 50000 * 0.01 + (income - 50000) * 0.02;
+            }
 
-          }
-    }
+
+            else if (income <= 100000) {
+                tax = 50000 * 0.01 + 25000 * 0.02 + (income - 75000) * 0.03;
+            }
+
+            else if (income <= 250000) {
+                tax = 50000 * 0.01 + 25000 * 0.02 + 25000 * 0.03 + (income - 100000) * 0.04;
+            }
+
+            else if (income <= 500000) {
+                tax = 50000 * 0.01 + 25000 * 0.02 + 25000 * 0.03 + 150000 * 0.04 + (income - 250000) * 0.05;
+            }
+
+            else if (income > 500000) {
+                tax = 50000 * 0.01 + 25000 * 0.02 + 25000 * 0.03 + 150000 * 0.04 + 250000 * 0.05 + (income - 500000) * 0.06;
 
 
+
+            }
+
+            System.out.println("Your tax is : " + tax);
+
+            input.close();
+
+
+        }}
