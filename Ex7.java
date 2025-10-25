531655a044d1388c949292d52a7b47d6166bd16f
bayat <sara.bayat1916@gmail.com>
2025-10-26 02:17:59 +0330

    E_6_5

--- Ex7.java
+++ Ex7.java
@@ -13,5 +13,25 @@ public class Ex7 {
         double max = -Double.MAX_VALUE;
         int count = 0;
 
+
+        while (in.hasNextDouble()) {
+               double num = in.nextDouble();
+            sum = sum + num;
+                 if (num < min) min = num;
+               if (num > max) max = num;
+            count++;
+        }
+
+        double average = 0;
+        if (count > 0) average = sum / count;
+        double range = max - min;
+
+        System.out.println("Average: " + average);
+
+        System.out.println("Smallest: " + min);
+        System.out.println("Largest: " + max);
+        System.out.println("Range: " + range);
+
+        in.close();
     }
 }
