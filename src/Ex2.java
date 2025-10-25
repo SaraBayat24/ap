2c64912605f7ebf4dbe4c88db87c2172183b8191
bayat <sara.bayat1916@gmail.com>
2025-10-26 01:52:09 +0330

    E_5_15

--- src/Ex2.java
+++ src/Ex2.java
@@ -1,6 +1,4 @@
-
-
-    import java.util.Scanner;
+import java.util.Scanner;
 
     public class Ex2 {
 
@@ -14,25 +12,15 @@
 
             if (income <= 50000) {
                 tax = income * 0.01;
-            }
-
-            else if (income <= 75000) {
+            } else if (income <= 75000) {
                 tax = 50000 * 0.01 + (income - 50000) * 0.02;
-            }
-
-            else if (income <= 100000) {
+            } else if (income <= 100000) {
                 tax = 50000 * 0.01 + 25000 * 0.02 + (income - 75000) * 0.03;
-            }
-
-            else if (income <= 250000) {
+            } else if (income <= 250000) {
                 tax = 50000 * 0.01 + 25000 * 0.02 + 25000 * 0.03 + (income - 100000) * 0.04;
-            }
-
-            else if (income <= 500000) {
+            } else if (income <= 500000) {
                 tax = 50000 * 0.01 + 25000 * 0.02 + 25000 * 0.03 + 150000 * 0.04 + (income - 250000) * 0.05;
-            }
-
-            else if (income > 500000) {
+            } else if (income > 500000) {
                 tax = 50000 * 0.01 + 25000 * 0.02 + 25000 * 0.03 + 150000 * 0.04 + 250000 * 0.05 + (income - 500000) * 0.06;
             }
 
@@ -41,9 +29,6 @@
             input.close();
         }
     }
-
-
-
 
 
 
