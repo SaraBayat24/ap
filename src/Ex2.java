8c7d08aaf07f47c72a03e3c145c66411a4e7675b
bayat <sara.bayat1916@gmail.com>
2025-10-26 01:22:35 +0330

    E_5_15

---
+++ src/Ex2.java
@@ -0,0 +1,50 @@
+
+
+    import java.util.Scanner;
+
+    public class Ex2 {
+
+        public static void main(String[] args) {
+
+            Scanner input = new Scanner(System.in);
+
+            System.out.print("Enter your income : ");
+            double income = input.nextDouble();
+            double tax = 0;
+
+            if (income <= 50000) {
+                tax = income * 0.01;
+            }
+
+            else if (income <= 75000) {
+                tax = 50000 * 0.01 + (income - 50000) * 0.02;
+            }
+
+            else if (income <= 100000) {
+                tax = 50000 * 0.01 + 25000 * 0.02 + (income - 75000) * 0.03;
+            }
+
+            else if (income <= 250000) {
+                tax = 50000 * 0.01 + 25000 * 0.02 + 25000 * 0.03 + (income - 100000) * 0.04;
+            }
+
+            else if (income <= 500000) {
+                tax = 50000 * 0.01 + 25000 * 0.02 + 25000 * 0.03 + 150000 * 0.04 + (income - 250000) * 0.05;
+            }
+
+            else if (income > 500000) {
+                tax = 50000 * 0.01 + 25000 * 0.02 + 25000 * 0.03 + 150000 * 0.04 + 250000 * 0.05 + (income - 500000) * 0.06;
+            }
+
+            System.out.println("Your tax is : " + tax);
+
+            input.close();
+        }
+    }
+
+
+
+
+
+
+
