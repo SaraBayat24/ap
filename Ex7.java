e9491a35f8a27f48177caa1769b9d454b9136a3f
bayat <sara.bayat1916@gmail.com>
2025-10-26 02:16:34 +0330

    E_6_3

---
+++ Ex7.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Ex7 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter floating-point numbers (in one line): ");
+
+
+
+    }
+}
