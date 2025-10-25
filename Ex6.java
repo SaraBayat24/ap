25ddd3182730d709053c3fc10725039231a975de
bayat <sara.bayat1916@gmail.com>
2025-10-26 02:11:01 +0330

    E_6_2

--- Ex6.java
+++ Ex6.java
@@ -2,34 +2,71 @@ import java.util.Scanner;
 
 public class Ex6 {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
 
-        Scanner input = new Scanner(System.in);
+        System.out.print("how many numbers do you want to enter: ? ");
+        int tedad = in.nextInt();
 
-        System.out.print("How many numbers ? ");
-        int count = input.nextInt();
+        System.out.println("enter the numbers: ");
+        int adad = in.nextInt();
 
+        int min = adad;
+        int max = adad;
 
-        System.out.print("Enter number 1: ");
-        int num = input.nextInt();
+        int zoj = 0;
+        int fard = 0;
 
-        int smallest = num;
-        int largest = num;
+        if (adad % 2 == 0)
+            zoj = 1;
+        else
+            fard = 1;
 
-        for (int i = 2; i <= count; i++) {
-            System.out.print("Enter number " + i + ": ");
-            num = input.nextInt();
+        int sum = adad;
+        String sumString = "" + sum;
 
-            if (num < smallest) {
-                smallest = num;
+        int ghabli = adad;
+        String tekrari = "";
+
+        for (int i = 2; i <= tedad; i++) {
+            adad = in.nextInt();
+
+            if (adad < min)
+                min = adad;
+
+            if (adad > max)
+                max = adad;
+
+            if (adad % 2 == 0)
+                zoj = zoj + 1;
+            else
+                fard = fard + 1;
+
+            sum = sum + adad;
+            sumString = sumString + " " + sum;
+
+            if (adad == ghabli) {
+                if (!tekrari.contains("" + adad)) {
+                    if (tekrari.equals(""))
+                        tekrari = "" + adad;
+                    else
+                        tekrari = tekrari + " " + adad;
+                }
             }
-            if (num > largest) {
-                largest = num;
-            }
 
+            ghabli = adad;
+        }
 
-            System.out.println("Smallest number: " + smallest);
-            System.out.println("Largest number: " + largest);
+        System.out.println("min: " + min);
+        System.out.println("max: " + max);
+        System.out.println("tedad zoj: " + zoj);
+        System.out.println("tedad fard: " + fard);
+        System.out.println("sum: " + sumString);
 
-            input.close(); }
+        if (tekrari.equals(""))
+            System.out.println("adad tekrari vojood nadare");
+        else
+            System.out.println("adad haye tekrari: " + tekrari);
+
+        in.close();
     }
 }
