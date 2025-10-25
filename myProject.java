64555ff84973f4f94a71f24a78dc0e852511e6e0
bayat <sara.bayat1916@gmail.com>
2025-10-25 23:55:24 +0330

    E_5_2

--- myProject.java
+++ myProject.java
@@ -1,14 +1,41 @@
-import java.io.File;
+// Write a program that reads a floating-point number and prints “zero” if the number
+//is zero. Otherwise, print “positive” or “negative”. Add “small” if the absolute value
+//of the number is less than 1, or “large” if it exceeds 1,000,000.
+
+
 import java.util.Scanner;
 
 public class myProject {
 
-    static int[][] board = new int[9][9];
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
-    public static void main(String[] args) throws Exception {
+        System.out.print("Enter floating-point number : ");
+        double x = input.nextDouble();
+
+        if (x == 0){
+            System.out.print("the number you entered is zero ");
+        }
+          else if (x > 0){
+            System.out.print("the number you entered is positive ");}
+
+            else {
+                System.out.print("the number you entered is negative ");
+              }
+
+          if(x < 0){ x = x * -1;}
+
+            if ( x < 1){
+            System.out.print("  small ");
+        }
+
+        if (x > 1000000){
+            System.out.print("larg ");
+        }
+
+input.close();
+
+          }
+    }
 
 
-
-
-
-    }}
