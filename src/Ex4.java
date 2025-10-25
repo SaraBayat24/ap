4f4e42bcf33d2c0eaf9b55d8d5ca1eb36f53ccfe
bayat <sara.bayat1916@gmail.com>
2025-10-26 01:37:38 +0330

    E_6_1_a

--- src/Ex4.java
+++ src/Ex4.java
@@ -3,9 +3,13 @@ public class Ex4 {
 
     public static void main(String[] args) {
 
+        int sum = 0;
+        int i = 2;
 
+        while (i <= 100) {
+            sum = sum + i;
+            i = i + 2;
+        }
+            System.out.println("Sum of even numbers from 2 to 100 is: " + sum);
 
-
-
-
-}
+}}
