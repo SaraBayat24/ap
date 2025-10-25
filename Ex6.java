037d2492912d15149db9175523815b0ae5ce7998
bayat <sara.bayat1916@gmail.com>
2025-10-26 02:00:08 +0330

    E_6_2

--- Ex6.java
+++ Ex6.java
@@ -8,5 +8,13 @@ public class Ex6 {
         System.out.print("How many numbers ? ");
         int count = input.nextInt();
 
+
+        System.out.print("Enter number 1: ");
+        int num = input.nextInt();
+
+        int smallest = num;
+        int largest = num;
+
+
     }
 }
