c6ed1152162a5d13c59c628f294826371ec80332
bayat <sara.bayat1916@gmail.com>
2025-10-26 01:49:01 +0330

    E_6_1_e

--- src/Ex5.java
+++ src/Ex5.java
@@ -4,6 +4,10 @@ public class Ex5 {
 
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = input.nextInt();
 
 
 
