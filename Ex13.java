6f0b490a6394789ee1a6066c655ac8896519948e
bayat <sara.bayat1916@gmail.com>
2025-10-26 02:31:15 +0330

    E_6_18

---
+++ Ex13.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Ex13 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("enter size: ");
+        int n = in.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        in.close();
+    }
+}
