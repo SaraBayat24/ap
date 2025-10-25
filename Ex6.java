2196631a78f03ecddcdcb1e6c9f9ed8cc3f651ef
bayat <sara.bayat1916@gmail.com>
2025-10-26 01:59:52 +0330

    E_6_2

--- Ex6.java
+++ Ex6.java
@@ -8,27 +8,5 @@ public class Ex6 {
         System.out.print("How many numbers ? ");
         int count = input.nextInt();
 
-        System.out.print("Enter number 1: ");
-        int num = input.nextInt();
-
-        int smallest = num;
-        int largest = num;
-
-        for (int i = 2; i <= count; i++) {
-            System.out.print("Enter number " + i + ": ");
-            num = input.nextInt();
-
-            if (num < smallest) {
-                smallest = num;
-            }
-            if (num > largest) {
-                largest = num;
-            }
-        }
-
-        System.out.println("Smallest number: " + smallest);
-        System.out.println("Largest number: " + largest);
-
-        input.close();
     }
 }
