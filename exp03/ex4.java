71e730dfc4fe078f8ac8e2045223edde59d8ecff
bayat <sara.bayat1916@gmail.com>
2025-11-13 02:58:40 +0330

    E3_3

---
+++ exp03/ex4.java
@@ -0,0 +1,35 @@
+package exp03;
+
+
+import java.util.Scanner;
+
+public class ex4 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("تعداد سطرهای ماتریس را وارد کنید (m): ");
+        int m = input.nextInt();
+        System.out.print("تعداد ستون‌های ماتریس را وارد کنید (n): ");
+        int n = input.nextInt();
+
+        int[][] matrix = new int[m][n];
+        int[][] transpos = new int[n][m];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
