757c4011db1334311a42d4f6d0c131c3c19666b8
bayat <sara.bayat1916@gmail.com>
2025-11-13 03:55:02 +0330

    E4_4

---
+++ exp04/main4.java
@@ -0,0 +1,35 @@
+package exp04;
+
+import java.util.Scanner;
+
+public class main4{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+  System.out.print("یک رشته وارد کنید : ");
+    String text = input.nextLine();
+
+  String result = "";
+
+   for (int i = 0; i < text.length(); i++) {
+        char ch = text.charAt(i);
+
+   if (ch >= 'a' && ch <= 'z') {
+                result += (char)(ch - 32);
+                           }
+
+      else if (ch >= 'A' && ch <= 'Z') {
+                result += (char)(ch + 32);
+                   }
+
+         else {
+                result += ch;
+               }
+            }
+
+
+   System.out.println(result);
+
+        input.close();
+    }
+}
